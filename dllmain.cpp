a23d68bdaa31afd1033199061aaf5fbf5f5dbfb0
Aeyth <85146374+Aeyth8@users.noreply.github.com>
2024-09-07 19:12:25 -0400

    Update dllmain.cpp
    Removed placeholder and added proper GBA handling

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,23 +8,32 @@ std::wstring LOG_Path = L" ";
 
 bool Initialized = false;
 
+// Converts a wide string into a normal string
+std::string Revertion(const std::wstring& wideStr) {
+	std::string WideString;
+	WideString.assign(wideStr.begin(), wideStr.end());
+	return WideString;
+}
+
 void Initialize() {
 	WCHAR EXEPath[MAX_PATH] = { 0 };
 	GetModuleFileName(NULL, EXEPath, MAX_PATH);
 
 	std::wstring DirectoryPath(EXEPath);
-	GBA = uintptr_t(GetModuleHandleW(NULL));
 	size_t EndDL = DirectoryPath.find_last_of(L"\\/");
-
+	std::wstring ShippingEXE;
 
 	if (EndDL != std::wstring::npos) {
 		// Remove the filename part to get the directory path
 		DirectoryPath = DirectoryPath.substr(0, EndDL + 1); // Keep the backslash
+		ShippingEXE = EXEPath + EndDL + 1;
 	}
 
+	GBA = uintptr_t(GetModuleHandleW(ShippingEXE.c_str()));
 	LOG_Path = DirectoryPath + L"Debug.log";
 
 	InitLog(LOG_Path);
+	DebugLog("INFO", Revertion(ShippingEXE));
 	DebugLog("INFO", "The Global Base Address [GBA] is " + std::to_string(GBA) + "\n\n");
 }
 
